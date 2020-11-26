0c7baecb3e50c0682422973b89a8b6addb96c6f5
danish-lucifer <74283994+danish-lucifer@users.noreply.github.com>
2020-11-26 10:50:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ function fun1(){
     if(str1=='Fahrenheit'){
         str2=(str*(9/5)+32);
         console.log(str2,'str2');
-        str=str+ ' C';
-        str2=str2+ ' F';
+        str=str+ ' \xB0C';
+        str2=str2+ ' \xB0F';
     }
     if(str1=='Celcius'){
         str2=((str-32)*(5/9));
         console.log(str2,'str22');
-        str=str+' F';
-        str2=str2+ ' C';
+        str=str+' \xB0F';
+        str2=str2+ ' \xB0C';
     }
     document.getElementById("inp1").value=str;
     document.getElementById('inp2').value=str2;
